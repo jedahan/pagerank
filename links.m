750eb7f41fda0aaf30e0f82d575da5b7dea96987
Jonathan Dahan <jedahan@gmail.com>
2010-02-10 12:02:36 -0500

    its alive

--- links.m
+++ links.m
@@ -1,36 +1,46 @@
+% FIXME 'warning: range error for conversion to character value'
+
 % make a cellarray of all the pages
 allpages = fopen('allpages.txt');
-pages{1} = fgetl(allpages)
+pages{1} = fgetl(allpages);
 while ~feof(allpages)
-    pages{end+1} = fgetl(allpages);
+  pages{end+1} = fgetl(allpages);
 end
 fclose(allpages);
 
+% initialize matrix
 b = zeros(length(pages), length(pages));
 
-href_regex = 'ahref=["'']([^"'']+)html["'']';
-
-% for each page
+% for each page,
 for page_num=1:length(pages)
-  page = fscanf(fopen(strrep(pages{page_num},"http://","")) ,'%s',Inf);
-  % our fscanf removes spaces lol
-  hrefs = regexpi(page, href_regex, 'match');
+  page = pages(page_num){1,1};
+
+  % capture all the hrefs.
+  webpage = fscanf(fopen(strrep(page,"http://","")) ,'%s',Inf);
+  hrefs = regexp(webpage, 'ahref=["'']([^"'']+html)["'']', 'tokens');
+
   % for each href
   for href_num=1:length(hrefs)
-    href = hrefs(href_num){1,1};
-    link = substr(href, 8, length(href)-8);
+    href = hrefs(href_num){1,1}{1,1};
 
-    % replace ../ with directory above
-    page(1:strfind(page,'/')(length(strfind(link,'../'))));
+    % replace first character in relative path with full path
+    href = regexprep(href,'^[/\w]',page(1:findstr(page,'/')(end)),'once');
 
-    if strfind(link,'http://')~=1
-      link = regexprep(link,'', 'http://$1');
+    % replace any number of ../s with the full path
+    if regexpi(href,'^\.\./')
+        ups = strfind(href,'../');
+        downs = strfind(page,'/');
+        href = strcat(page(1:downs(end-length(ups))), href(ups(end)+3:end));
     end
 
-    pages
+    % prepend http if its not found,
+    if isempty(strfind(href, 'http://'))
+      href = strcat('http://',href);
+    end
 
-    b(find(strcmp(pages,page)), find(strcmp(pages,link))) = 1;
+    % mark the appropriate entry in b
+    b(find(strcmp(pages,page)), find(strcmp(pages,href))) = 1;
   end
 end
 
-
+b
